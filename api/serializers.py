c03e6ae9139bfb06e7327ae767e4744851cde606
yousefshref <yb2005at@gmail.com>
2025-02-26 18:48:11 +0200

    api

--- api/serializers.py
+++ api/serializers.py
@@ -5,9 +5,19 @@ from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
     role = serializers.ReadOnlyField(source='sales.role')
-    class Meta():
+    
+    class Meta:
         model = User 
         fields = '__all__'
+    
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        user = User(**validated_data)
+        if password:
+            user.set_password(password)
+        user.save()
+        return user
+
 
 
 class ShippingCourierSerializer(serializers.ModelSerializer):
